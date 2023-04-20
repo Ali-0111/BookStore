80fea032b266ac4126c4e7e04bb29b536d79e765
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-21 01:25:19 +0430

    actions and state for categorSlice added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* eslint-disable no-console */
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import bookSlice, { addBook, removeBook } from './books/booksSlice';
+import categorySlice, { checkStatus } from './categories/categorySlice';
+
+const rootReducer = combineReducers({
+  bookReducer: bookSlice.reducer,
+  categoryReducer: categorySlice.reducer,
+});
 
 const store = configureStore({
-  reducer: bookSlice.reducer,
+  reducer: rootReducer,
 });
 
 // initial state
-console.log(store.getState());
+console.log('initial state for 2 reducer', store.getState());
 
 // add 4 book
 store.dispatch(addBook('Math'));
 store.dispatch(addBook('Physics'));
 store.dispatch(addBook('Biology'));
 store.dispatch(addBook('Chemistry'));
-console.log(store.getState());
+console.log('added 4 book', store.getState().bookReducer);
 
 // remove one book, book remined: 3
 store.dispatch(removeBook('Math'));
-console.log(store.getState(), 'Math removed');
+console.log('Math removed', store.getState().bookReducer);
 
+//                       section book slice end
+
+store.dispatch(checkStatus());
+console.log('status for catergory:', store.getState().categoryReducer);
 export default store;
+/* eslint-enable no-console */
