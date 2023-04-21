3540057474b2079aae42a9e8b338d3108a2ce11f
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-21 10:23:15 +0430

    additional console.log() removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import bookSlice, { addBook, removeBook } from './books/booksSlice';
-import categorySlice, { checkStatus } from './categories/categorySlice';
+import bookSlice from './books/booksSlice';
+import categorySlice from './categories/categorySlice';
 
 const rootReducer = combineReducers({
   bookReducer: bookSlice.reducer,
@@ -12,23 +11,4 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
-// initial state
-console.log('initial state for 2 reducer', store.getState());
-
-// add 4 book
-store.dispatch(addBook('Math'));
-store.dispatch(addBook('Physics'));
-store.dispatch(addBook('Biology'));
-store.dispatch(addBook('Chemistry'));
-console.log('added 4 book', store.getState().bookReducer);
-
-// remove one book, book remined: 3
-store.dispatch(removeBook('Math'));
-console.log('Math removed', store.getState().bookReducer);
-
-//                       section book slice end
-
-store.dispatch(checkStatus());
-console.log('status for catergory:', store.getState().categoryReducer);
 export default store;
-/* eslint-enable no-console */
