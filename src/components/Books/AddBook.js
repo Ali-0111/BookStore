cea133717374cf6542bbe54c44d1a4d1564cb884
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-21 12:58:22 +0430

    Unique id added , remove function works based on unique id now

--- src/components/Books/AddBook.js
+++ src/components/Books/AddBook.js
@@ -7,9 +7,14 @@ const AddBook = () => {
   const [bookName, setBookName] = useState('');
   const [authorName, setAuthorName] = useState('');
   const update = (bookName, authorName) => (
-    dispatch(addBook({
-      title: bookName, author: authorName, category: 'NA', id: bookName + authorName,
-    })));
+    dispatch(addBook(
+      {
+        title: bookName,
+        author: authorName,
+        category: 'NA',
+        id: Date.now().toString(),
+      },
+    )));
   const submitHandler = (e) => {
     e.preventDefault();
     update(bookName, authorName);
