44d8a54e40a02fc796e8b7fe6989a0e1787e15f4
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-28 17:30:18 +0430

    Header style added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,17 @@ import { NavLink, Outlet } from 'react-router-dom';
 const Header = () => (
   <>
     <header className="main-head">
-      <h1>Bookstore CMS</h1>
-      <nav>
-        <ul className="links">
-          <li><NavLink className="link" to="/">Books</NavLink></li>
-          <li>
-            <NavLink className="link" to="/categories">Categories</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="first-part-head">
+        <h1>Bookstore CMS</h1>
+        <nav>
+          <ul className="links">
+            <li><NavLink className="link" to="/" activeClassName="active">Books</NavLink></li>
+            <li>
+              <NavLink className="link" to="/categories">Categories</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div className="profile">
         <span className="material-icons">person</span>
       </div>
